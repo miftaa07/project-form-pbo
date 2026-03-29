11955c02e6d443fe0740e94ac9e31af307f0e611
Oribelrenata <oribel_geraldine_31tkj@student.smktelkom-mlg.sch.id>
2026-03-29 20:28:54 +0700

    update file index

--- process.php
+++ process.php
@@ -31,11 +31,33 @@
 </head>
 <body>
 
-<h2>Form Output</h2>
+<div class="card">
+    <h2>Form Output</h2>
 
-<div class="container">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstname = $_POST['firstname'];
+        $lastname  = $_POST['lastname'];
+        $phone     = $_POST['phone'];
+        $address   = $_POST['address'];
+    ?>
 
+        <input type="text" value="<?= $firstname ?>" readonly>
+        <input type="text" value="<?= $lastname ?>" readonly>
+        <input type="text" value="<?= $phone ?>" readonly>
+        <textarea readonly><?= $address ?></textarea>
 
+        <div class="result">
+            <p><strong>Hi,</strong> my name is <?= $firstname . " " . $lastname; ?></p>
+            <p><strong>Phone:</strong> <?= $phone; ?></p>
+            <p><strong>Address:</strong> <?= $address; ?></p>
+        </div>
+
+    <?php
+    } else {
+        echo "Data tidak ditemukan!";
+    }
+    ?>
 </div>
 
 </body>
