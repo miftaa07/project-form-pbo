c77b35952249a1f40b2e5c801d6a418d4ec60bf0
Oribelrenata <oribel_geraldine_31tkj@student.smktelkom-mlg.sch.id>
2026-03-29 18:43:18 +0700

    tambah file index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Form Pbo</title>
+</head>
+<body>
+    <h2>Form Input</h2>
+
+    <form method="POST" action="">
+        Nama: <input type="text" name="nama"><br><br>
+        Email: <input type="email" name="email"><br><br>
+        <button type="submit">Submit</button>
+    </form>
+
+
+
+</body>
+</html>
